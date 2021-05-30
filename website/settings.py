fafaf1b2ec912021fec95a0759ddb120ba1b1444
Vicky <biswajeetp08@gmail.com>
2021-05-30 21:43:19 +0530

    debug True

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mbgv3p2l01nt2j+d+pk*)%v==h0b508))@*ngeiyv+(d87ce8s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','pool-of-polls.herokuapp.com']
 
